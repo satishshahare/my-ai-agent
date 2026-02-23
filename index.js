89d3c0404190f45ddeaa5490b88827fa8c44ddd1
satishshahare <satishshahare@sakshichemicals.com>
2026-02-23 18:41:05 +0530

    Update index.js

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const express = require("express");
 const app = express();
 
 app.get("/", (req, res) => {
-  res.send("Hello! AI Agent is Working!");
+  res.send("Hello! Working!");
 });
 
 const PORT = process.env.PORT || 3000;
